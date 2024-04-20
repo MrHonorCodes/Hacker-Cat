7685823746cf2ba1800038955566add5aa4a4db3
Ana Alvarez <147456318+aea-ana@users.noreply.github.com>
2024-04-20 16:30:02 -0500

    Add files via upload

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseGameMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseGameMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseGameMenu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseGameMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void RestartButton()
+    {
+        //SceneManager.LoadScene(sceneID);
+    }
+    
+    public void ReturnToTitleButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
